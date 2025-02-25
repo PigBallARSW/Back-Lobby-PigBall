953503938db51f5ca0c85278c64f62a7c85ec490
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-02-25 10:23:13 -0500

    feat: first base

---
+++ src/main/java/co/edu/eci/pigball/lobby/LobbyApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eci.pigball.lobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyApplication.class, args);
+	}
+
+}
